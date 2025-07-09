0dbb822dbf95b7568420475a4c47dba56ae3ad6a
Dakota <dakota.w.brown@silvercastledigital.com>
2025-07-09 15:31:17 -0500

    come back after a long while

--- GameServer/obj/Debug/net9.0/GameServer.AssemblyInfo.cs
+++ GameServer/obj/Debug/net9.0/GameServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15f75d75302bc00d61d639df1088746f11ecc43f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+601c3c89a67917762f4f3a1c1ae24328075f846e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
